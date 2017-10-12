d93e81df4613798c93013543da0e07b85f8904ec
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-12 14:36:16 +1100

    updated the room controller & set up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,16 @@
 Rails.application.routes.draw do
-  get 'room/index'
-
-  get 'room/new'
-
-  get 'room/create'
-
-  get 'room/listing'
-
-  get 'room/pricing'
-
-  get 'room/description'
-
-  get 'room/photo_upload'
-
-  get 'room/amenities'
-
-  get 'room/location'
-
-  get 'room/update'
-
   root :to => 'pages#home'
 
   devise_for :users
 
   resources :users, only: [:show]
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
 end

71efb6fa1e69faa2b91f788c357a529e84a761bd
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-10-12 14:07:41 +1100

    generate model Room

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   validates :fullname, presence: true, length: {maximum: 50}
+
+  has_many :rooms
 end
